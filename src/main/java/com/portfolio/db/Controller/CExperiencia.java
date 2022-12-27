504705cc75efbf85e58c3d0edf37c40badaca4c1
miguelalgranti <108410813+miguelalgranti@users.noreply.github.com>
2022-12-27 15:55:22 -0300

    arreglueti

--- src/main/java/com/portfolio/db/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/db/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://portfolio-ap-ma.web.app)
+@CrossOrigin(origins = "https://portfolio-ap-ma.web.app")
 public class CExperiencia {
    @Autowired
    SExperiencia sExperiencia;
