cb8f3de33272fffa2ea50ec749a1a9144abf6f05
Miguel M. Algranti <108410813+miguelalgranti@users.noreply.github.com>
2024-01-22 02:12:45 -0300

    Update CExperiencia.java

--- src/main/java/com/portfolio/db/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/db/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://portfolio-ap-ma.web.app")
+@CrossOrigin(origins = "https://portfolio-ap-ma.web.app", "https://miguelalgranti.online")
 public class CExperiencia {
    @Autowired
    SExperiencia sExperiencia;
